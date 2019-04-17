c4167ff74de5c4deb41ca8f9b2bd0aa6ae8778ec
Mikhail Tcirulnikov <lexxone@ya.ru>
2019-04-16 21:58:48 -0400

    add angular ui app

---
+++ my-app/src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './service/api.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent implements OnInit {
+  title = 'Guid generator';
+  count : number = 1;
+  guids: string[];
+
+  constructor(private apiService: ApiService){}
+
+  ngOnInit() {
+    console.log('The component is initialized');
+  }
+
+  getGuid(): void {
+    this.apiService.getGuid().subscribe(
+      res =>{
+      this.guids = [res];
+    },
+    err => {
+      console.log('HTTP Error', err);
+      window.alert('api does not exists');
+     });
+  }
+
+  getGuids(count: number): void {
+    this.apiService.getGuids(count).subscribe(
+      res =>{
+      this.guids = res;
+    },
+     err => {
+      console.log('HTTP Error', err);
+      window.alert('api does not exists');
+     });
+  }
+}
